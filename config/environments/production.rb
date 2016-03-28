e80f6b9839bd9d34aa05d45a6e60ac232fb4428b
David Williams <david.williams@webascender.com>
2016-03-28 14:17:01 -0400

    adds mailgun to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,11 +76,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.delivery_method = :sendmail
-  # Defaults to:
-  config.action_mailer.sendmail_settings = {
-    :location => '/usr/sbin/sendmail',
-    :arguments => '-i -t'
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'YOUR_APPS_DOMAIN', #eg: 'yourappname.herokuapp.com'
+    :authentication => :plain,
   }
   config.action_mailer.default_url_options = { :host => 'movie-theater.herokuapp.com' }
   config.action_mailer.perform_deliveries = true
