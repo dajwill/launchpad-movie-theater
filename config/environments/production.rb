acfb9280c650743cdcd68a2f38a59f16a5cd5090
David Williams <david.williams@webascender.com>
2016-03-28 14:22:50 -0400

    fix for production mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'movie-theater.herokuapp.com'
+    :domain         => 'movie-theater.herokuapp.com',
     :authentication => :plain,
   }
   config.action_mailer.default_url_options = { :host => 'movie-theater.herokuapp.com' }
